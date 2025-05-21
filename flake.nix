027786a217d9c3d1220bb0f8678d178aa19ace85
Chinmay D. Pai <chinmaydpai@gmail.com>
2025-05-21 11:27:06 +0530

    chore: switch to rust-beta toolchain
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- flake.nix
+++ flake.nix
@@ -10,15 +10,17 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    rust-overlay,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [(import rust-overlay)];
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
@@ -28,22 +30,30 @@
         buildInputs = with pkgs; [
           openssl
         ];
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           inherit buildInputs nativeBuildInputs;
 
           packages = with pkgs; [
-            rust-bin.stable.latest.default
+            (pkgs.rust-bin.beta.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
             rust-analyzer
             gcc
           ];
 
-          RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
+          RUST_SRC_PATH = "${
+            pkgs.rust-bin.beta.latest.default.override {
+              extensions = [ "rust-src" ];
+            }
+          }/lib/rustlib/src/rust/library";
         };
 
-        packages.default = let
-          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        in
+        packages.default =
+          let
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+          in
           pkgs.rustPlatform.buildRustPackage {
             pname = manifest.name;
             version = manifest.version;
